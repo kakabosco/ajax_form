efdc1d10f9b9ba298e12ade9b37e6f11264435e2
Kaique Bosco <kaique.bosco@hotmail.com>
2023-08-15 11:08:30 -0300

    fetch + error handling

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// Pure JS
+
 // document.addEventListener("DOMContentLoaded", function () {
 //     document.getElementById("btn-cep").addEventListener("click", function () {
 //         // AJAX - Asynchronous JavaScript and XML
@@ -15,24 +17,57 @@ $(document).ready(function () {
 
     $("#btn-cep").click(function () {
         const cep = $("#cep").val();
-        const endpoint = `https://viacep.com.br/ws/${cep}/json/`;
+        const endpoint = `https://viacep.com.br/ws/${cep}/json`;
         const button = $(this);
 
         $(button).find("i").addClass("d-none");
         $(button).find("span").removeClass("d-none");
 
-        $.ajax(endpoint).done(function (answer) {
-            setTimeout(function () {
-                const logradouro = answer.logradouro;
-                const bairro = answer.bairro;
-                const cidade = answer.localidade;
-                const estado = answer.uf;
+        // AJAX
+
+        // $.ajax(endpoint).done(function (answer) {
+        //     setTimeout(function () {
+        //         const logradouro = answer.logradouro;
+        //         const bairro = answer.bairro;
+        //         const cidade = answer.localidade;
+        //         const estado = answer.uf;
+        //         const endereco = `${logradouro}, ${bairro} - ${cidade} - ${estado}`;
+        //         $("#endereco").val(endereco);
+
+        //         $(button).find("i").removeClass("d-none");
+        //         $(button).find("span").addClass("d-none");
+        //     }, 1000);
+        // });
+
+        // Fetch API
+        fetch(endpoint)
+            .then(function (answer) {
+                return answer.json();
+            })
+            .then(function (json) {
+                const logradouro = json.logradouro;
+                const bairro = json.bairro;
+                const cidade = json.localidade;
+                const estado = json.uf;
                 const endereco = `${logradouro}, ${bairro} - ${cidade} - ${estado}`;
                 $("#endereco").val(endereco);
+            })
+            .catch(function (error) {
+                alert("Ocorreu um erro ao buscar o endereço, tente novamente.");
+            })
+            .finally(function () {
+                setTimeout(function () {
+                    $(button).find("i").removeClass("d-none");
+                    $(button).find("span").addClass("d-none");
+                }, 500);
+            });
+    });
 
-                $(button).find("i").removeClass("d-none");
-                $(button).find("span").addClass("d-none");
-            }, 1000);
-        });
+    $("#form-request").submit(function (e) {
+        e.preventDefault();
+
+        if ($("#name").val().length == 0) {
+            throw new Error("O campo nome é obrigatório");
+        }
     });
 });
